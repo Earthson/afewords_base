ae840b67e9ba3ee892b6739cc0cedc5a14792fdf
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-04 21:43:04 +0800

    fix

--- afewords/www/handlers/basehandler.py
+++ afewords/www/handlers/basehandler.py
@@ -38,43 +38,4 @@ class BaseHandler(RequestHandler):
         return
 
     def get_error_html(self, status_code=500, **kwargs):
-        '''
-        user = self.current_user
-        if user is not None:
-            AFUser = SuperUser(user)
-        else:
-            AFUser = None
-        error_info = dict()
-        error_info = kwargs
-        error_info['url'] = self.request_url
-        error_info['code'] = status_code
-        error_info['debug'] = AFWConfig.afewords_debug
-        if 'title' not in error_info:
-            title = '迷路了 - 子曰'
-        if "des" not in error_info:
-            if status_code >=500:
-                error_info['des'] = '抱歉，服务器出错！请您将这里的信息复制并作
-为反馈提交给我们，我们将尽快修复这个问题！'
-            else:
-                error_info['des'] = "开发人员未给出具体描述！"
-        if "next_url" not in error_info:
-            error_info['next_url'] = '/'
-        if "exc_info" not in error_info:
-            if "my_exc_info" not in error_info:
-                if status_code >=500:
-                    error_info['exc_info'] = '抱歉，服务器出错！'
-                else:
-                    error_info['exc_info'] = '错误栈未传入错误输出程序！'
-            else:
-                error_info['exc_info'] = error_info['my_exc_info']
-        if "reason" not in error_info:
-            error_info['reason'] = [error_info['des']]
-        if status_code >= 500:
-            logging.error(error_info['exc_info'])
-            logging.error('Url Error %s' % self.request.uri)
-
-
-        return self.render_string("error.html", user=AFUser, title=title, error=error_info)
-
-        '''
-        pass
+        return self.write(str(status_code) + '<br/>' + str(kwargs))
