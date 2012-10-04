daadcb450d37ed62f599c668ecb3e6d67465eaa6
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-04 22:49:01 +0800

    page __template_file__

--- afewords/www/handlers/loginhandler.py
+++ afewords/www/handlers/loginhandler.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 from basehandler import BaseHandler
-
+from pages.pages import LoginPage
 from generator import id_generator, index_generator
 
 
 class LoginHandler(BaseHandler):
     def get(self):
-        return self.write('Hello world!')
+        page = LoginPage(self)
+        return page.render()
