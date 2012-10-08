d66d0ce02a01dad299aec999aa538fc9a7b9620d
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-08 19:25:20 +0800

    fix authority

--- afewords/aflib/authority.py
+++ afewords/aflib/authority.py
@@ -6,11 +6,11 @@ obj.authority_verify(usr, **env) is a method to get permission level to spec obj
 use test_auth() to judge whether you have permission
 '''
 
-A_WRITE = 1 << 5
-A_POST = 1 << 4
-A_DEL = 1 << 3
-A_MANAGE = 1 << 2
 A_READ = 1
+A_WRITE = 1 << 2
+A_POST = 1 << 3
+A_DEL = 1 << 4
+#A_MANAGE = 1 << 5
 
 
 def set_auth(self, auth):
