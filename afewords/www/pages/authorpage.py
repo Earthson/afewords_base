5414a1536c97fd56f011e147bfc79d87d36d6df4
deju <deju.tu@gmail.com>
2012-10-14 18:42:07 +0800

    add authorpage

---
+++ afewords/www/pages/authorpage.py
@@ -0,0 +1,51 @@
+#coding=utf-8
+
+from basepage import BasePage, with_attr
+
+
+@with_attr
+class AuthorPage(BasePage):
+    __template_file__ = 'afewords-bloger.html'
+    doc = {
+        'bloger_type' : 'blog',
+        'author' : {},  # see [[ author ]] in data_format
+    }
+
+
+@with_attr
+class AuthorBlogPage(AuthorPage):
+    doc = {
+        'bloger_type' : 'blog',
+        'blog_list' : [],   # see [[ blog_list ]] in data_format
+    } 
+
+
+@with_attr
+class AuthorLikePage(AuthorPage):
+    doc = {
+        'bloger_type' : 'like',
+        'like_list' : [],   # see [[ like_list ]] in data_format
+    }
+
+
+@with_attr
+class AuthorBookPage(AuthorPage):
+    doc = {
+        'bloger_type' : 'book',
+        'book_list' : [],   # see [[ book_list ]] in data_format
+    }
+
+
+@with_attr
+class AuthorFollowPage(AuthorPage):
+    doc = {
+        'bloger_type' : 'follow',
+        'follow_list' : [], # see [[ follow_list ]] in data_format
+    }
+
+@with_attr
+class AuthorAboutPage(AuthorPage):
+    doc = {
+        'bloger_type' : 'about',
+        'about' : {},   # see [[ article ]] in data_format
+    }
