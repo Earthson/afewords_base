ec67c6ef50c39ecc54ff34ca52e9e624e7722c06
deju <deju.tu@gmail.com>
2012-10-15 15:14:03 +0800

    tool-paging.html is ok

---
+++ afewords/www/pages/toolpages.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+
+from basepage import BasePage, with_attr
+
+@with_attr
+class PagingPage(BasePage):
+    '''
+    create paging html
+    '''
+    __template_file__ = 'tool-paging.html'
+    doc = {
+        'current_page' : 1, # int , current_page
+        'page_list' : [],   # list, [1,2,3,4,5]
+        'base_url' : '',    # unicode, like /like?page=
+    }
+
+
