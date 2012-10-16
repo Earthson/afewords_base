465647935ec9d2c920ad9f26fc02501b7a00d965
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-16 21:24:10 +0800

    fix

--- afewords/www/handlers/basehandler.py
+++ afewords/www/handlers/basehandler.py
@@ -25,13 +25,6 @@ def with_login(operate):
         return
     return wrapper
 
-def url_with_para(baseurl, paradoc):
-    url = baseurl
-    if paradoc:
-        url = url + r'?' + '&'.join([str(ek)+'='+str(ev) 
-                            for ek, ev in paradoc.items()])
-    return url
-
 class BaseHandler(RequestHandler):
     def redirect_with_para(self, url, paradoc):
         self.redirect(url_with_para(url, paradoc))
