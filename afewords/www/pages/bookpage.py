e52c1ca4494c9c81b15fbfc7e5ee5e3fd50a46f8
deju <deju.tu@gmail.com>
2012-10-16 18:36:13 +0800

    fix

--- afewords/www/pages/bookpage.py
+++ afewords/www/pages/bookpage.py
@@ -4,8 +4,25 @@ from basepage import BasePage, with_attr
 
 
 @with_attr
-class BookPage(BasePage):
+class BookBasePage(BasePage):
     __template_file__ = 'book-base.html'
-    doc = {}
+    doc = {
+        'book':{},  # dict, see [[ book ]] in data_format
+        'catalog_html': '', # unicode
+        'want_page': 'cover',   # unicode, cover, summary, catalog, chapter
+    }
 
 
+@with_attr
+class BookPage(BookBasePage): 
+    __template_file__ = 'book-index.html'
+    doc = {
+        'isedit': False,    # bollean
+    }
+
+@with_attr
+class BookChapter(BookBasePage):
+    __template_file__ = 'book-chapter.html'
+    doc = {
+        'current_chapter': dict,    # see [[ chapter ]] in data_format 
+    }
