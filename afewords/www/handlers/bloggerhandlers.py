4ea453c9ba96a7c991c4f755ad56fa25ec11b0e0
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-20 19:22:57 +0800

    fix

---
+++ afewords/www/handlers/bloggerhandlers.py
@@ -0,0 +1,59 @@
+#coding=utf-8
+
+from basehandler import *
+from pages.bloggerpage import BloggerBlogPage
+from afutils.page_utils import page_split
+
+
+class BloggerBlogPara(BaseHandlerPara):
+    paradoc = {
+        'page' : 1,
+        'tag' : 'default',
+    }
+    
+    def read(self):
+        try:
+            self['page'] = int(self.handler.get_esc_arg('page', self['page']))
+        except:
+            self['page'] = 1
+        self['tag'] = self.handler.get_esc_arg('tag', self['tag'])
+
+    def verify(self):
+        return 0
+
+class BloggerBlogHandler(BaseHandler):
+
+    def get(self, uid=None):
+        from user import User
+        usr = self.current_user
+        if uid is None:
+            if usr is None:
+                self.redirect('/login')    
+                return
+            author = usr
+        else:
+            author = User.by_id(uid)
+            if author is None:
+                self.send_error(404)
+                return
+        paras = BloggerBlogPara(self)
+        page = BloggerBlogPage(self)
+        page['current_page'] = paras['page']
+        if usr:
+            page['author'] = usr.as_viewer(author)
+        else:
+            page['author'] = author.basic_info
+        page['tag_list'] = author.lib.tag_lib['alltags']
+        page['current_tag'] = paras['tag']
+        blogs_info = author.blogs_info_view_by(usr, page['current_tag'])
+        page['blog_list'], page['page_list'] = page_split(blogs_info,
+                        page['current_page'], 10)
+        #for paging support 
+        paradoc = dict()
+        if paras['tag'] != 'default':
+            paradoc['tag'] = paras['tag']
+        page['urlparas'] = paradoc
+        page['baseurl'] = self.request_url
+        page.page_init()
+        page.render()
+        return
