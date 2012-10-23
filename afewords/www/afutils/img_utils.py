7e0ed3c2f99f5704d0accd440bb3c0a1f19977a7
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-23 17:22:43 +0800

    add img_utils

---
+++ afewords/www/afutils/img_utils.py
@@ -0,0 +1,36 @@
+
+import Image
+import tempfile
+
+image_type_doc = {
+    'image/gif' : 'gif',
+    'image/jpeg' : 'jpg',
+    'image/pjpeg' : 'jpg',
+    'image/bmp' : 'bmp',
+    'image/png' : 'png',
+    'image/x-png' : 'png',
+    'image/x-icon' : 'ico',
+}
+
+def upload_img(uploaded_file, wl=100, wh=2000, hl=100, hh=2000):
+    '''uploaded_file from handler, return a tmp file'''
+    image_type_list = ['image/gif', 'image/jpeg', 'image/pjpeg', 
+                            'image/bmp', 'image/png', 'image/x-png']
+    if uploaded_file['content_type'] not in image_type_list:
+        return 1, 'Unsupported file type'
+    img_format = image_type_doc[uploaded_file['content_type']]
+    if len(uploaded_file['body']) > 4 * 1024 * 1024:
+        return 2, 'Uploaded file too large'
+    tmp_file = tempfile.NamedTemporaryFile(delete=True)   
+    tmp_file.write(uploaded_file['body'])
+    tmp_file.seek(0)
+    try:
+        img_tmp = Image.open(tmp_file.name)
+    except IOError, e:
+        tmp_file.close()
+        return 3, 'Invalid image'
+    tmp_file.close()
+    if ((wl <= img_tmp.size[0] <= wh) and 
+                (hl <= img_tmp.size[1] <= hh)) is False:
+        return 4, 'Invalid Width or Height'
+    return 0, img_tmp, img_format 
