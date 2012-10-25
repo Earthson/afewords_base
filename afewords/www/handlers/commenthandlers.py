82d94e452d14da20aae89b8d4c4f609ba0a32d76
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-25 16:25:07 +0800

    fix

--- afewords/www/handlers/commenthandlers.py
+++ afewords/www/handlers/commenthandlers.py
@@ -13,17 +13,23 @@ class CommentGetPara(BaseHandlerPara):
 
     def read(self):
         self.paradoc = dict([(ek, self.handler.get_esc_arg(ek, ev))
-                                for ek, ev in self.praradoc.items()])
+                                for ek, ev in self.paradoc.items()])
+
 
 class CommentGetHandler(BaseHandler):
 
     def post(self):
+        from generator import generator
         handler_json = GetCommentJson(self)
         handler_paras = CommentGetPara(self)
         article_obj = generator(handler_paras['article_id'],
                             type_trans(handler_paras['article_type']))
+        if article_obj is None:
+            handler_json.by_status(1)
+            handler_json.write()
+            return #Article not Exist
         comments_info = article_obj.comments_info_view_by(self.current_user)
-        handler_json['info'] = comments_info
+        handler_json['comment_list'] = comments_info
         handler_json.by_status(0)
         handler_json.write()
         return
