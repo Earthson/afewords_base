fa14ba28179894716d2ab0bb1f4d4d5efef11749
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-26 18:35:04 +0800

    add settingshandlers.py

---
+++ afewords/www/handlers/settingshandlers.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+
+from basehandler import *
+from pages.settingspage import UserSettingInvitePage
+from pages.settingspage import UserSettingPasswordPage
+from pages.settingspage import UserSettingDomainPage
+from pages.settingspage import UserSettingAvatarPage
+from pages.settingspage import UserSettingFollowPage
+from pages.settingspage import UserSettingTagPage
+from pages.settingspage import UserSettingFollowerPage
+from pages.settingspage import UserSettingNoticePage
+from pages.settingspage import UserSettingDraftPage
