91d8deb23312ae833fe53b8c717c403e1f6146d6
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-29 00:58:31 +0800

    fix

--- afewords/www/handlers/bookhandlers.py
+++ afewords/www/handlers/bookhandlers.py
@@ -11,13 +11,23 @@ class BookHandlerPara(BaseHandlerPara):
         'load': 'cover',    # unicode, cover or summary or catalog
     }
 
+    def read(self):
+        BaseHandlerPara.read(self)
+        if self['load'] not in ('cover', 'summary', 'catalog', 'chapter'):
+            self['load'] = 'cover'
+
 
 class BookHandler(BaseHandler):
-    def get(self):
+    def get(self, bid):
+        from article.catalog import Catalog
         handelr_paras = BookHandlerPara(self)
-        handler_page = BookPage
+        handler_page = BookPage(self)
         usr = self.current_user
-        #todo Earthson
+        catalog_obj = Catalog.by_id(bid)
+        if catalog_obj is None:
+            self.send_error(404)
+            return
+        handler['book'] = catalog_obj.basic_info
         handler.by_status(0)
         handler.render()
         return
