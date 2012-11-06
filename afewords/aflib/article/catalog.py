3b89258e088e08d65711a392092bce78d41bafce
Earthson Lu <Earthson.Lu@gmail.com>
2012-11-06 19:50:34 +0800

    fix

--- afewords/aflib/article/catalog.py
+++ afewords/aflib/article/catalog.py
@@ -446,7 +446,7 @@ class Catalog(DataBox):
     def as_env():
         def getter(self):
             ans = dict()
-            ans['env_type'] = self.self.cls_name
+            ans['env_type'] = self.cls_name
             ans['env_id'] = self.uid
             ans['entity'] = self.as_env_info
             return ans
