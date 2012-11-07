24dc4566a92cf557ffff5220bb173283119217b9
deju <deju.tu@gmail.com>
2012-11-07 22:39:55 +0800

    fix

---
+++ afewords/www/pages/af_bookpage.py
@@ -0,0 +1,34 @@
+from basepage import *
+
+@with_attr
+class AFBookPage(BasePage):
+    __template_file__ = "afewords-book/book-index.html"
+    ''' for url /afewords-book '''
+    doc = {
+        'page_type': 'book',
+        'subpage_type': 'index',
+        'current_page' : 1,
+        'paging_html': '',  # for paging
+        'book_list': None,  # list
+    }
+
+@with_attr
+class AFBookMyselfPage(BasePage):
+    __template_file__ = "afewords-book/book-myself.html"
+    doc = {
+        'page_type': 'book',
+        'subpage_type': 'myself',
+        'current_page': 1,
+        'pagind_html': '',  # for paging
+        'book_list': None,  # list
+    }
+
+
+@with_attr
+class AFBookCreatePage( BasePage ):
+    __template_file__ = "afewords-book/book-create.html"
+    ''' for url /book-create '''
+    doc = {
+        'page_type': 'book',
+        'subpage_type': 'create',
+    }
