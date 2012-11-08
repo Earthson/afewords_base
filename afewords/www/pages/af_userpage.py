5d50a4f4703e9a02812eaec3c0d29235984118a7
deju <deju.tu@gmail.com>
2012-11-08 20:16:05 +0800

    fix;

---
+++ afewords/www/pages/af_userpage.py
@@ -0,0 +1,16 @@
+
+from basepage import *
+
+@with_attr
+class AFUserLibPage(BasePage):
+    ''' for url /user-lib '''
+    __template_file__ = "afewords-user/user-index.html"
+    doc = {
+        'page_type': 'user',
+        'subpage_type': 'index',
+        'title': '人群 - 子曰',
+        'user_list': [],    # list 
+        'current_page': 1,
+        'paging_html': '',
+    }
+
