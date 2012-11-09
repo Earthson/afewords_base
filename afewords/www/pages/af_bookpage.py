6cb43bf0da436c775d958779bd0880dfb29f96cb
deju <deju.tu@gmail.com>
2012-11-09 22:17:46 +0800

    fix

--- afewords/www/pages/af_bookpage.py
+++ afewords/www/pages/af_bookpage.py
@@ -6,6 +6,7 @@ class AFBookPage(BasePage):
     __template_file__ = "afewords-book/book-index.html"
     ''' for url /afewords-book '''
     doc = {
+        'title': '知识谱 - 子曰',
         'page_type': 'book',
         'subpage_type': 'index',
         'current_page' : 1,
@@ -18,6 +19,7 @@ class AFBookMyselfPage(BasePage):
     __template_file__ = "afewords-book/book-myself.html"
     ''' for url /user-book '''
     doc = {
+        'title': '我的知识谱 - 子曰'
         'page_type': 'book',
         'subpage_type': 'myself',
         'current_page': 1,
@@ -31,6 +33,7 @@ class AFBookCreatePage( BasePage ):
     __template_file__ = "afewords-book/book-create.html"
     ''' for url /book-create '''
     doc = {
+        'title': '创建知识谱 - 子曰'
         'page_type': 'book',
         'subpage_type': 'create',
     }
