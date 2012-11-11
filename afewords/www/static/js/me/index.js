7f34a8a77202a5a215da9329984b4973b49292e6
deju <deju.tu@gmail.com>
2012-11-11 22:07:40 +0800

    fix

--- afewords/www/static/js/me/index.js
+++ afewords/www/static/js/me/index.js
@@ -19,9 +19,76 @@ $("#head_user_block").bind({
         }
 });
 
-$(document.body).bind('click', function(){
-    $("#head_user").hide();
+$(document.body).live('click', function(event){
+    var $head_user = jQuery(document.getElementById("head_user"));
+    if($head_user.css("display") == "block") $head_user.hide();
+    
+    /****** bind login *********/
+    var target = event.target,
+        $target = jQuery(target);
+    if(target.nodeName == "BUTTON"){
+        // afewords-in
+        if(AFWUser.subpage_type == "login"){
+                    
+        }
+    }
+    
 });
+
+
+jQuery(document.getElementById("login_do")).live('click', function(event){
+    /****
+        contains login, reg, reset page 
+    ****/
+    var target = event.target,
+        $target = jQuery(target);
+
+    if(target.nodeName == "BUTTON"){
+            
+    }
+
+});
+
+(function(){
+    /******* login page *********/
+    if(AFWUser['subpage_type'] == "login"){
+        //alert(0);
+        jQuery(document.getElementById("login_do")).find("form").bind('click', function(event){
+            var target = event.target, $target = jQuery(target), that = this, $form = jQuery(that);
+            var $process, ret, mes;
+            event.stopPropagation();
+            if(target.nodeName != "BUTTON") return false;
+            $process = $form.find("span.login-process");
+            mes = $form.FormToDict();
+            ret = Global_Funs["login"]["check"](mes);
+            if(ret[0] < 0){
+                $process.html(ret[1]).css("color", "red");
+                event.preventDefault();
+                return false;              
+            }
+            return true;
+        })    
+    }
+    
+})();
+
+
+Global_Funs = {
+    "login": {
+                "check": function(paras){
+                            var res = { status: -1, info: ''};
+                            var email_reg = (/^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/);
+                            if(!paras['email'] || !email_reg.test(paras['email']))
+                                return [-1, '请您填写正确的邮箱！'];
+                            //alert(0);
+                            if(!paras['pwd'])
+                                return [-1, '请填写密码！'];
+                            if(!paras['token'])
+                                return [-1, '请填写验证码！'];
+                            return [0, '']
+                         }    
+            }
+}
 
 
 });
--- afewords/www/static/js/me/tool.js
+++ afewords/www/static/js/me/tool.js
@@ -1,5 +1,157 @@
 (function($){
 // tools for the all pages
+$.extend({
+    encode: function(value){
+                var html_encode_dict = {'&': "&amp", '<':"&lt;", '>':"&gt;", "'":"&#39;", '"':'&quot;' };
+                if(typeof(value) == "string"){
+                    return ret_fun(value);
+                }else{
+                    for(var ii in value){
+                        if(typeof value[ii] == "string")  value[ii] = ret_fun(value[ii]);        
+                    }    
+                    return value;
+                }
+    
+                function ret_fun(str){
+                    return str.replace(/'|"|>|</g, function(tt){
+                        return html_encode_dict[tt];
+                    })  
+                }
+            },
+    decode: function(value){
+                var html_decode_dict = {"&amp":'&', "&lt;":'<', "&gt;":'>', "&#39;":"'", '&quot;':'"' };
+                if(typeof value == "string"){
+                    return ret_fun(value);
+                }else{
+                    for(var ii in value){
+                        if(typeof value[ii] == "string")  value[ii] = ret_fun(value[ii]);        
+                    }
+                return value;    
+                }
+    
+                function ret_fun(str){
+                    return str.replace(/&amp;|&lt;|&gt;|&#39;|&quot;/g, function(tt){
+                        return html_decode_dict[tt];
+                    });   
+                }
+            },
+    getCookie: function(name){
+                    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+                    return r ? r[1] : undefined;
+            },
+    getQuery: function(name){
+                    var reg = new RegExp("(^|\\?|&)"+ name +"=([^&]*)(\\s|&|$)", "i");
+                    if (reg.test(location.href)) return unescape(RegExp.$2.replace(/\+/g, " ")); return "";
+            },
+    postJSON: function(url, args, callback_before,callback_success , callback_error ){
+                    args._xsrf = jQuery.getCookie("_xsrf");
+                    $.ajax({
+                        url: url, 
+                        data: $.param(args), 
+                        dataType: "json", 
+                        type: "POST",
+                        contentType:"application/x-www-form-urlencoded; charset=utf-8",
+    		                  beforeSend:function(){  callback_before();  },
+                        success:function(response) {   if(callback_success) callback_success(response);   }, 
+    		                  error: function(response, textStatus) { callback_error(textStatus);   }
+	                   });
+            }	
+    
+});
+
+jQuery.fn.extend({
+    FormToDict: function(){
+                    var fields = this.serializeArray();
+                    var json = {}
+                    for (var i = 0; i < fields.length; i++) {
+                        json[fields[i].name] = fields[i].value;
+                    }
+                    if (json.next) delete json.next;
+                    return json;
+                },
+    DivToDict: function(){
+                    var json = {};
+                    var root_this = $(this),
+                        self_this,
+                        input_type,
+                        key,
+                        value;
+                    root_this.find('input').each(function(){
+                        self_this = $(this); input_type = self_this.attr("type");
+                        key = self_this.attr("name"); value = jQuery.trim(self_this.val());
+                        switch(input_type){
+                            case 'radio':
+                                if(self_this.attr('checked') != undefined ){
+                                    json[key] = value;
+                                }
+                                break;
+                            case 'checkbox':
+                                if(self_this.attr("checked") != undefined){
+                                    if(key in json){ 
+                                        json[key].push(value);                    
+                                    }else{
+                                        json[key] = []; json[key].push(value);                    
+                                    }			     
+                                }
+                                break;
+                            default:
+                                json[key] = value;
+                                break;  
+                        }
+                    });
+                    root_this.children().find('textarea').each(function(){
+                        json[$(this).attr("name")] = $.trim($(this).val());	
+                    });
+                    root_this.children().find('select').each(function(){
+                        json[$(this).attr("name")] = $.trim($(this).val());	
+                    });
+                    return json;
+                }
+});
+
+
+pop_page = function(_width , _height , _content){
+    var page_height = $(document).height(),
+        page_width = $(document).width(),
+        screen_height = $(window).height(),
+        screen_width = $(window).width();
+
+    if(screen_height < _height)	screen_height = _height;
+    if(screen_width < _width)	screen_width = _width;
+    var top_length = ( screen_height - _height )/2 - 30;
+    if(top_length <=0) top_length = 0;
+    var left_length = ( screen_width - _width )/2 - 15;
+    if(left_length <=0) left_length = 0;
+	
+    var $pop_overlay = jQuery(document.getElementById("pop-overlay")),
+        $pop_wrap = jQuery(document.getElementById("pop-wrap")),
+        $pop_content = jQuery(document.getElementById("pop-content")); 
+               
+    $pop_overlay.css({"height": parseInt(page_height) + "px","width":parseInt(page_width) + "px"})
+                .fadeIn(200,function(){
+                        $pop_wrap.css({"width":parseInt(_width) + "px", 
+			                             "height": parseInt(_height) +"px",
+			                             "top":parseInt(top_length) + "px",
+			                             "left":parseInt(left_length)+"px"
+			                             }).fadeIn(200);
+                        $pop_content.css({"width":parseInt(_width)-20 +"px","height":parseInt(_height)-20 + "px"});
+                        if(typeof _content == "object"){
+                            $pop_content.html('').append(_content);			
+                        }else{
+                            $pop_content.html(_content);
+                        }
+                });
+    setTimeout(function(){}, 0);
+    return $pop_content;
+}
+
+/******************pop page close **************************************/
+pop_page_close = function(){
+    $("#pop-wrap").fadeOut(200,function(){
+        $("#pop-overlay").fadeOut(200);
+        $("#pop-content").html('');
+    });	
+}
 
 
 
