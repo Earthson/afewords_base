9d977937cdb8883e288193f2f069a3f34477fc45
deju <deju.tu@gmail.com>
2012-11-11 22:25:59 +0800

    fix

--- afewords/www/static/js/me/index.js
+++ afewords/www/static/js/me/index.js
@@ -36,16 +36,25 @@ $(document.body).live('click', function(event){
 });
 
 
-jQuery(document.getElementById("login_do")).live('click', function(event){
+jQuery(document.getElementById("login_do")).bind('click', function(event){
     /****
         contains login, reg, reset page 
     ****/
     var target = event.target,
-        $target = jQuery(target);
-
-    if(target.nodeName == "BUTTON"){
-            
+        $target = jQuery(target),
+        that = this;
+        $that = jQuery(that);
+    //alert(0)
+    if(target.nodeName != "BUTTON") return false;
+    var $process = $target.siblings("span.login-process"), ret;
+    var mes = $that.DivToDict();
+    ret = Global_Funs[AFWUser.subpage_type]["check"](mes);
+    if(ret[0] < 0){
+        $process.html(ret[1]).css("color", "red");
+        return false;
     }
+    jQuery.postJSON();
+    // can ajax
 
 });
 
@@ -85,9 +94,26 @@ Global_Funs = {
                                 return [-1, '请填写密码！'];
                             if(!paras['token'])
                                 return [-1, '请填写验证码！'];
-                            return [0, '']
+                            return [1, '']
                          }    
-            }
+            },
+    "reg": {
+                "url": '/reg',
+                "check": function(paras){
+                            var email_reg = (/^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/);
+                            if(!paras['email']|| !email_reg.test(paras['email'])){ 
+                                return [-1, '请您填写正确的邮箱！']; 
+                            }
+                            if(!paras['name']){ return [-1, '请您填写姓名！'];  }	
+                            if(paras['name'].length < 2){ return [-1, '姓名至少为两个字！'];  }
+                            
+                            if(!paras['pwd'] || paras['pwd'].length < 4){ return [-1, '请您设置登陆密码，至少为四位！'];	}	
+                            if(paras['pwd'] != paras['pwd_again']){ return [-1, '确认密码错误！'];	}
+                            if(!paras['token']){ return [-1, '请填写验证码！']; }
+                            return [1, '']
+                }
+                   
+        }
 }
 
 
