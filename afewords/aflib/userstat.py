823b23e568c1978d2029020ed7b41c70cf1dd9d0
Earthson Lu <Earthson.Lu@gmail.com>
2012-11-17 22:51:59 +0800

    add userstat

---
+++ afewords/aflib/userstat.py
@@ -0,0 +1,66 @@
+from databox.afdocument import AFDocument
+from databox.mongokit_utils import with_conn
+from databox.databox import *
+
+from emmongodict.emmongodict import EmMongoDict
+
+import time
+
+@with_conn
+class UserStatDoc(AFDocument):
+    __collection__ = 'UserStatDB'
+
+    structure = {
+        'article_views' : {
+            #article_id'
+            basestring : {
+                'type':basestring, #article_type
+                'count':int, #article_view count
+                'last_time':float, #last view time, gen by time.time()
+            }
+        }
+    }
+    required_fields = []
+    default_values = {
+        'article_views' : dict,
+    }
+
+
+class UserStatMongo(EmMongoDict):
+    datatype = UserStatDoc
+
+    db_info = {
+        'db' : 'afewords',
+        'collection' : 'UserStatDB',
+    }
+
+    @property
+    def article_views(self):
+        return self.sub_dict('article_views')
+
+    def view_article(self, article_obj):
+        '''try to view article'''
+        view_doc = self.article_views.sub_dict(article_obj.uid)
+        time_last = view_doc['last_time']
+        time_now = time.time()
+        if time_last is None:
+            doc_new = {
+                'type' : article_obj.cls_name,
+                'count' : 1,
+                'last_time' : time_now,
+            }
+            view_doc.set_all(doc_new)
+            return True
+        elif time_now - time_last < 2000:
+            return False
+        view_doc['last_time'] = time_now
+        print view_doc.inca('count')
+        return True
+
+@with_mapper
+class UserStat(DataBox):
+    datatype = UserStatDoc
+
+    mapper = {
+        'article_views' : True,
+    }
