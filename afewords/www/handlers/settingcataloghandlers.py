74b431e4480f89fe29c74548746f3893a5afd75d
Earthson Lu <Earthson.Lu@gmail.com>
2012-11-18 21:52:22 +0800

    fix

--- afewords/www/handlers/settingcataloghandlers.py
+++ afewords/www/handlers/settingcataloghandlers.py
@@ -161,7 +161,8 @@ class RecArticleToBookHandler(BaseHandler):
             handler_json.write()
             return #section not exist
         handler_json['book_title'] = book.name
-        handler_json['chapter_title'] = book.get_node_dict(node_id)['title']
+        handler_json['chapter_title'] = book.get_node_dict(
+                    handler_para['node_id'])['title']
         handler_json['article_title'] = article_obj.name
         handler_json['relation_id'] = rr.uid
         handler_json.by_status(0)
