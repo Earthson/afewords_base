5d19aa55bbebaa3d5b65154513a40f85be7f63c1
Earthson Lu <Earthson.Lu@gmail.com>
2012-11-19 21:47:27 +0800

    fix

--- afewords/tools/do_init.py
+++ afewords/tools/do_init.py
@@ -4,5 +4,6 @@ import os
 for each in os.popen('ls *.py'):
     if each == 'fix.py\n' or each == 'do_init.py\n':
         continue
-    info = os.popen('python2 ' + each)
-    print(''.join(e for e in info))
+    print('running...: %s' % each[:-1])
+    info = os.popen('python2 ' + each[:-1])
+    print('end: %s' % each[:-1])
--- afewords/tools/fix.py
+++ afewords/tools/fix.py
@@ -2,5 +2,7 @@
 
 import os
 for each in os.popen('ls fix_*.py'):
-    info = os.popen('python2 ' + each) 
-    print(''.join(e for e in info))
+    print('running...: %s' % each[:-1])
+    info = os.popen('python2 ' + each[:-1]) 
+    print('end: %s' % each[:-1])
+    #print(''.join(e for e in info))
