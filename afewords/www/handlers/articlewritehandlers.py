b62320405548a0f380815baf600bc55e2a06bcc7
Earthson Lu <Earthson.Lu@gmail.com>
2012-11-23 17:04:01 +0800

    fix

--- afewords/www/handlers/articlewritehandlers.py
+++ afewords/www/handlers/articlewritehandlers.py
@@ -95,7 +95,7 @@ def article_env_init(handler, handler_paras, handler_json):
         acls = cls_gen(handler_paras['article_type'])
         if not acls or not issubclass(acls, Article):
             return 11#Invalid Article Type
-        if not test_auth(handler.env.authority_verify(usr), A_POST):
+        if not test_auth(handler.env.authority_verify(usr), A_WRITE):
             return 12#Permission Denied
         handler.article_obj = acls()
         if handler.article_obj is None:
@@ -169,6 +169,7 @@ class ArticleUpdateHandler(BaseArticleUpdateHandler):
         handler_paras = ArticleUpdatePara(self)
         handler_json = UpdateArticleJson(self)
         usr = self.current_user
+        print(usr.account_status)
         status = self.env_init(handler_paras, handler_json)
         if status != 0:
             handler_json.by_status(status)
