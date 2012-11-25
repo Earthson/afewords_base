f3a7c7b87062dce63c513d34014ad73f488e4125
Earthson Lu <Earthson.Lu@gmail.com>
2012-11-26 00:59:33 +0800

    fix

--- afewords/www/afconfig.py
+++ afewords/www/afconfig.py
@@ -1,4 +1,5 @@
 #coding=utf-8
+import os
 af_conf = {
     'debug' : True,
     'needinvite' : True,
@@ -8,4 +9,5 @@ af_conf = {
     #'main_mail' : r'afewords@ipv6.earthson.net',
     'invitation_limit' : 50,
     'autoescape': None,
+    'root_dir': os.path.dirname(__file__),
 }
