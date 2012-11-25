a04d72849ee325cd545a281f3a92528955fbfe50
Earthson Lu <Earthson.Lu@gmail.com>
2012-11-25 23:12:01 +0800

    fix

--- afewords/www/afconfig.py
+++ afewords/www/afconfig.py
@@ -2,10 +2,10 @@
 af_conf = {
     'debug' : True,
     'needinvite' : True,
-    #'main_url' : r'http://www.afewords.com',
-    'main_url' : r'http://ipv6.earthson.net',
-    #'main_mail' : r'afewords@afewords.com',
-    'main_mail' : r'afewords@ipv6.earthson.net',
+    'main_url' : r'http://www.afewords.com',
+    #'main_url' : r'http://ipv6.earthson.net',
+    'main_mail' : r'afewords@afewords.com',
+    #'main_mail' : r'afewords@ipv6.earthson.net',
     'invitation_limit' : 50,
     'autoescape': None,
 }
