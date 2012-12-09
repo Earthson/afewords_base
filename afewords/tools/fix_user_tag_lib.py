8253c0215403adac5ce90b8d10c32b6de6f48b19
Earthson Lu <Earthson.Lu@gamil.com>
2012-12-09 21:31:52 +0800

    fix

--- afewords/tools/fix_user_tag_lib.py
+++ afewords/tools/fix_user_tag_lib.py
@@ -13,6 +13,6 @@ for each in user_all:
             continue
         if not tmp[tag]:
             continue
-        if not isinstance(tmp[tag][0], list):
-            tmp[tag] = [(e._id, e.release_time) for e in Blog.by_ids(tmp[tag])]
+        tmp[tag] = [(e._id, e.release_time) if not isinstance(e, list) else e
+                for e in Blog.by_ids(tmp[tag])]
     tag_lib.set_all(tmp)
