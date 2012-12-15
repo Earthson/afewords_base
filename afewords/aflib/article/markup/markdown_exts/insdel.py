8ee993848812fb5a12d40e934879e999d835e4c9
Earthson Lu <Earthson.Lu@gmail.com>
2012-12-16 01:28:03 +0800

    add files of extensions

---
+++ afewords/aflib/article/markup/markdown_exts/insdel.py
@@ -0,0 +1,14 @@
+import markdown
+
+DEL_RE = r'(--)(.+?)--'
+INS_RE = r'(\.\.)(.+?)\.\.'
+
+class InsDelExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        del_tag = markdown.inlinepatterns.SimpleTagPattern(DEL_RE, 'del')
+        md.inlinePatterns.add('del', del_tag, '>not_strong')
+        ins_tag = markdown.inlinepatterns.SimpleTagPattern(INS_RE, 'ins')
+        md.inlinePatterns.add('ins', ins_tag, '>del')
+
+def makeExtension(configs=None):
+    return InsDelExtension(configs)
