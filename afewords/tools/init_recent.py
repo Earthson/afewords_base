341f77253700273874f74cb1120f1a249f8ab7c3
Earthson Lu <Earthson.Lu@gmail.com>
2012-12-17 21:37:20 +0800

    fix

--- afewords/tools/init_recent.py
+++ afewords/tools/init_recent.py
@@ -5,7 +5,7 @@ from article.blog import Blog
 from article.catalog import Catalog
 from user import User
 
-recent_blogs.set_all([None for i in range(200)])
+recent_blogs.set_all([None for i in range(500)])
 blog_all = sorted([Blog(each) 
                 for each in Blog.datatype.find()], reverse=True)
 for each in blog_all:
@@ -14,14 +14,14 @@ for each in blog_all:
     recent_blogs.push(each._id)
     recent_blogs.pop_head()
 
-recent_books.set_all([None for i in range(200)])
+recent_books.set_all([None for i in range(500)])
 book_all = sorted([Catalog(each) 
                 for each in Catalog.datatype.find()], reverse=True)
 for each in book_all:
     recent_books.push(each._id)
     recent_books.pop_head()
 
-recent_users.set_all([None for i in range(200)])
+recent_users.set_all([None for i in range(500)])
 user_all = sorted([User(each) 
                 for each in User.datatype.find()], reverse=True)
 for each in user_all:
@@ -29,4 +29,4 @@ for each in user_all:
     recent_users.pop_head()
 
 unreg_users.set_all([None for i in range(5000)])
-recent_feedbacks.set_all([None for i in range(200)])
+recent_feedbacks.set_all([None for i in range(500)])
