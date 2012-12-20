bc82832b6bb5b2d72b8c3f567407c0072a260139
Earthson Lu <Earthson.Lu@gmail.com>
2012-12-20 21:24:31 +0800

    fix markdown mathjax extension from code https://github.com/mayoff/python-markdown-mathjax/blob/master/mdx_mathjax.py

--- afewords/aflib/article/markup/markdown_exts/mathjax.py
+++ afewords/aflib/article/markup/markdown_exts/mathjax.py
@@ -1,13 +1,20 @@
 import markdown
 
-_math_re = r'(?<!\\)(((\$\$?.+?(?<!\\)\$\$?)))'
-#_equation_re = r'(?<!\\)(((\$\$[\s\S]+?(?<!\\)\$\$)))'
+class MathJaxPattern(markdown.inlinepatterns.Pattern):
+
+    def __init__(self):
+        markdown.inlinepatterns.Pattern.__init__(self, r'(?<!\\)(\$\$?)(.+?)\2')
+
+    def handleMatch(self, m):
+        node = markdown.util.etree.Element('mathjax')
+        node.text = markdown.util.AtomicString(m.group(2) + m.group(3) + m.group(2))
+        return node
 
 class MathJaxExtension(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
         # Needs to come before escape matching because \ is pretty important in LaTeX
-        math_tag = markdown.inlinepatterns.SimpleTagPattern(_math_re, 'mathjax')
-        md.inlinePatterns.add('mathjax', math_tag, '<escape')
+        md.inlinePatterns.add('mathjax', MathJaxPattern(), '<escape')
+
 
 def makeExtension(configs=None):
     return MathJaxExtension(configs)
